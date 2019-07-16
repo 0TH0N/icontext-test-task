50055ecbe0d68bc89fa029ac65888de3f7da5c6a
0TH0N <jbator777@gmail.com>
2019-07-17 00:42:57 +0300

    Commit. Again.

--- src/autoload.php
+++ src/autoload.php
@@ -3,11 +3,8 @@
 $autoloadPath1 = __DIR__ . '/../../../autoload.php';
 $autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
 
-require_once $autoloadPath2;
-/*
 if (file_exists($autoloadPath1)) {
     require_once $autoloadPath1;
 } else {
     require_once $autoloadPath2;
 }
-*/
